260902a477baf71b0eef7c543874da9c6bc01001
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-17 19:29:13 +0100

    Add config description

--- conf/config.schema.js
+++ conf/config.schema.js
@@ -2,7 +2,8 @@ module.exports = {
   definition: {
     locale: {
       type: 'String',
-      default: 'en'
+      default: 'en',
+      description: 'The language to be used by the back-end server for log messages etc.'
     }
   }
 };

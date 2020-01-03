a2472adf59ed8196e53be7a897006c06ff588d84
Thomas Taylor <thomas.taylor@kineo.com>
2020-01-03 16:44:02 +0000

    Refactor for new load process

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-const Module = require('./lib/module');
-const Utility = require('./lib/utility');
-module.exports = { Module, Utility };
+const LangModule = require('./lib/langmodule');
+module.exports = LangModule;

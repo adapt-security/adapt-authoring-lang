32c6099a30050524ee2bca5034f2b6ed5cf11ecf
Thomas Taylor <thomas.taylor@kineo.com>
2020-01-10 11:48:28 +0000

    Convert config schema to json-schema format

--- conf/config.schema.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  definition: {
-    locale: {
-      type: 'String',
-      default: 'en',
-      description: 'The language to be used by the back-end server for log messages etc.'
-    }
-  }
-};
